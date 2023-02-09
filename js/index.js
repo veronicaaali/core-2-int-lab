5b35e69e88b5ec9d12f1e46037665cd3477f3e65
Veronica Li <lij500@newschool.edu>
2023-02-09 22:52:36 +0800

    part 1

---
+++ js/index.js
@@ -0,0 +1,6 @@
+document.querySelectorAll('.grid-container').forEach(grid => {
+  grid.addEventListener('click', event => {
+    console.log('navvin ', `/pages/${event.target.id}.html`)
+    window.location.href = `/pages/${event.target.id}.html`;
+  })
+})
